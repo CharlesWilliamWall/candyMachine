0cf961c978f47c9856844aba46a5339a8fe4c311
CharlesWilliamWall <101436320+CharlesWilliamWall@users.noreply.github.com>
2022-03-13 15:16:18 -0400

    13-mar-2022

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Projet01
+﻿using System.Security.Cryptography;
+
+namespace Projet01
 {
     public class Program
     {
@@ -6,33 +8,75 @@
         public static void Main()
         {
             //chargement des données
-            Candy[] candies = new Candy[25];
-            Data DataManager = new Data(); 
-            candies = DataManager.LoadCandies();
-            GetSelection();
-            GetCandy();
+            Data dataManager = new Data();
+            candies = dataManager.LoadCandies();
+            decimal Sommerecu = 0m;
+            Board.Print();
+            Console.Write("->");
+            while (true)
+            {
+                int selection = GetSelection();
+                Candy bonbon = GetCandy(selection);
+                Board.Print(bonbon.Name, selection , bonbon.Price);
+                Console.Write("->");
+                do
+                {
+                    Board.Print(received: Sommerecu);
+                    GetCoin();
+                    Sommerecu = Sommerecu + GetCoin();
+                } while (selection > 0);
+
+                if (bonbon.Stock == 0)
+                {
+                    Board.Print(bonbon.Name + " VIDE!");
+                }
+            }
         }
         public static int GetSelection()
         {
-            int selection = 0;
+            int selection;
             do
             {
-                Board.Print();
-                Console.Write("->");
                 selection = int.Parse(Console.ReadLine());
-            } while (selection > 0 && selection < 25);
-            
-            if (selection > 25 || selection < 0)
-            {
-                Console.WriteLine("entrer une nouvelle selection");
-                Console.Write("->");
-                selection = int.Parse(Console.ReadLine());
-            }
+            } while (selection < 0 || selection > 26);
             return selection;
         }
-        public static Candy[] GetCandy()
+        public static Candy GetCandy(int selection)
         {
-            return candies;
+            return candies[selection -1];
         }
+
+        public static decimal GetCoin()
+        {
+            bool IsCompleted;
+            bool IsCanceled;
+            // choix 0 = annule;
+            decimal[] choix = new decimal[6];
+            // int prix = choix;
+            decimal coin = 0;
+            choix[0] = coin;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("[0] = Annuler");
+                Console.WriteLine("[1] = 5c");
+                Console.WriteLine("[2] = 10c");
+                Console.WriteLine("[3] = 25c");
+                Console.WriteLine("[4] = 1$");
+                Console.WriteLine("[5] = 2$");
+                choix[1] = 0.05m;
+                choix[2] = 0.10m;
+                choix[3] = 0.25m;
+                choix[4] = 1.00m;
+                choix[5] = 2.00m;
+                choix[i] = decimal.Parse(Console.ReadLine());
+            }
+            return coin;
+        }
+
+
+
+
+
+
     }
 }
